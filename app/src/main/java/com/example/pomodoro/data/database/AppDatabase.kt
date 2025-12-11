a5342b1810440af61bb9dc0912b0d87bcaefbd25
Adán Romero <adanromeromarrero@gmail.com>
2025-12-11 12:27:26 +0100

    Sistema de gegeración de monedas funcional.

--- app/src/main/java/com/example/pomodoro/data/database/AppDatabase.kt
+++ app/src/main/java/com/example/pomodoro/data/database/AppDatabase.kt
@@ -7,8 +7,9 @@ import androidx.room.RoomDatabase
 import androidx.room.migration.Migration
 import androidx.sqlite.db.SupportSQLiteDatabase
 import com.example.pomodoro.data.model.PomodoroTask
+import com.example.pomodoro.data.model.User
 
-@Database(entities = [PomodoroTask::class], version = 4, exportSchema = false)
+@Database(entities = [PomodoroTask::class, User::class], version = 4, exportSchema = false)
 abstract class AppDatabase : RoomDatabase() {
     abstract fun taskDao(): TaskDao
     abstract fun userDao(): UserDao
@@ -17,14 +18,12 @@ abstract class AppDatabase : RoomDatabase() {
         @Volatile
         private var INSTANCE: AppDatabase? = null
 
-        // Migración de versión 1 a 2
         private val MIGRATION_1_2 = object : Migration(1, 2) {
             override fun migrate(database: SupportSQLiteDatabase) {
                 database.execSQL("ALTER TABLE tasks ADD COLUMN timeSpentInSeconds INTEGER NOT NULL DEFAULT 0")
             }
         }
 
-        // NUEVA Migración de versión 2 a 3
         private val MIGRATION_2_3 = object : Migration(2, 3) {
             override fun migrate(database: SupportSQLiteDatabase) {
                 database.execSQL("ALTER TABLE tasks ADD COLUMN progressNotes TEXT NOT NULL DEFAULT ''")
@@ -42,7 +41,6 @@ abstract class AppDatabase : RoomDatabase() {
                         totalNotesWritten INTEGER NOT NULL DEFAULT 0
                     )
                 """)
-                // Insertar usuario inicial
                 database.execSQL("INSERT OR IGNORE INTO user (id, coins) VALUES (1, 0)")
             }
         }
